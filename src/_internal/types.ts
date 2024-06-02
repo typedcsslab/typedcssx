9db8709eacf959905281c82f1743be089ed700fe
S.Watanabe <ocelot2828@gmail.com>
2024-06-02 12:20:29 +0900

    feat: Major update, adding margin auto types and units other etc.

--- src/_internal/types.ts
+++ src/_internal/types.ts
@@ -1,46 +1,86 @@
 import type { CSSColorNames } from './colors';
 
-type AbsoluteCSSUnit = 'cm' | 'mm' | 'Q' | 'in' | 'pc' | 'pt' | 'px';
-type RelativeCSSUnit = 'em' | 'rem' | 'ex' | 'ch' | 'lh' | 'rlh';
+type AbsoluteCSSUnit = 'px' | 'cm' | 'mm' | 'Q' | 'in' | 'pc' | 'pt';
+type LocalFontRelativeCSSUnit = 'cap' | 'ch' | 'em' | 'ex' | 'ic' | 'lh';
+type RootFontRelativeCSSUnit = 'rcap' | 'rch' | 'rem' | 'rex' | 'ric' | 'rlh';
 type ViewportCSSUnit = 'vh' | 'vw' | 'vmin' | 'vmax' | 'vb' | 'vi';
 type RespectCSSUnit = 'svw' | 'svh' | 'lvw' | 'lvh' | 'dvw' | 'dvh';
 type PercentageCSSUnit = '%';
 
 type CSSAbsoluteUnitValue = '0' | `${number}${AbsoluteCSSUnit}`;
-type CSSRelativeUnitValue = '0' | `${number}${RelativeCSSUnit}`;
+type CSSLocalFontRelativeUnitValue = '0' | `${number}${LocalFontRelativeCSSUnit}`;
+type CSSRootFontRelativeUnitValue = '0' | `${number}${RootFontRelativeCSSUnit}`;
 type CSSViewportUnitValue = '0' | `${number}${ViewportCSSUnit}`;
 type CSSRespectUnitValue = '0' | `${number}${RespectCSSUnit}`;
 type CSSPercentageUnitValue = '0' | `${number}${PercentageCSSUnit}`;
-type CSSNumericValue = CSSAbsoluteUnitValue | CSSRelativeUnitValue | CSSViewportUnitValue | CSSRespectUnitValue | CSSPercentageUnitValue | CSSVariableValue;
+type CSSGlobalValue = 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset';
+type CSSLengthSubValue = 'none' | 'max-content' | 'min-content' | 'fit-content' | `fit-content(${number}${CSSNumericValue})`;
+type CSSFontSizeSubValue = 'xx-small' | 'x-small' | 'small' | 'medium' | 'large' | 'x-large' | 'xx-large' | 'math';
+type CSSArithmeticOperations = '+' | '-' | '*' | '/';
+type CSSCalcExpressionFunctioin = `calc(${string} ${CSSArithmeticOperations} ${string})`;
+type CSSColumnsValue = `${CSSNumericValue | number}` | 'auto' | `${CSSNumericValue | number} auto` | `auto ${CSSNumericValue | number}` | 'auto auto';
 
-type CSSSizeValue<T extends CSSAbsoluteUnitValue | CSSRelativeUnitValue | CSSViewportUnitValue | CSSRespectUnitValue | CSSPercentageUnitValue> =
-  | `${T}`
-  | `${T} ${T}`
-  | `${T} ${T} ${T}`
-  | `${T} ${T} ${T} ${T}`;
+type CSSNumericValue =
+  | CSSAbsoluteUnitValue
+  | CSSLocalFontRelativeUnitValue
+  | CSSRootFontRelativeUnitValue
+  | CSSViewportUnitValue
+  | CSSRespectUnitValue
+  | CSSPercentageUnitValue
+  | CSSCalcExpressionFunctioin
+  | CSSVariableValue
+  | CSSGlobalValue;
+
+type CSSSizeValue<T extends string | number> = `${T}` | `${T} ${T}` | `${T} ${T} ${T}` | `${T} ${T} ${T} ${T}`;
+
+type CSSAbsoluteUnitAndAutoValue = CSSAbsoluteUnitValue | 'auto';
+type CSSRelativeUnitAndAutoValue = CSSLocalFontRelativeUnitValue | 'auto';
+type CSSViewportUnitAndAutoValue = CSSViewportUnitValue | 'auto';
+type CSSRespectUnitAndAutoValue = CSSRespectUnitValue | 'auto';
+type CSSPercentageUnitAndAutValue = CSSPercentageUnitValue | 'auto';
+
+type CSSEdgeSizeAbsoluteWithAutoValues = CSSSizeValue<CSSAbsoluteUnitAndAutoValue>;
+type CSSEdgeSizeRelativeWithAutoValues = CSSSizeValue<CSSRelativeUnitAndAutoValue>;
+type CSSEdgeSizeViewportWithAutoValues = CSSSizeValue<CSSViewportUnitAndAutoValue>;
+type CSSEdgeSizeRespectWithAutoValues = CSSSizeValue<CSSRespectUnitAndAutoValue>;
+type CSSEdgeSizePercentageWithAutoValues = CSSSizeValue<CSSPercentageUnitAndAutValue>;
+type CSSEdgeSizeCalcExpressionFunctioin = CSSSizeValue<CSSCalcExpressionFunctioin>;
+
+type CSSMarginEdgeSizeValues =
+  | CSSEdgeSizeAbsoluteWithAutoValues
+  | CSSEdgeSizeRelativeWithAutoValues
+  | CSSEdgeSizeViewportWithAutoValues
+  | CSSEdgeSizeRespectWithAutoValues
+  | CSSEdgeSizePercentageWithAutoValues
+  | CSSEdgeSizeCalcExpressionFunctioin
+  | CSSGlobalValue
+  | CSSVariableValue;
 
 type CSSEdgeSizeAbsoluteValues = CSSSizeValue<CSSAbsoluteUnitValue>;
-type CSSEdgeSizeRelativeValues = CSSSizeValue<CSSRelativeUnitValue>;
+type CSSEdgeSizeRelativeValues = CSSSizeValue<CSSLocalFontRelativeUnitValue>;
 type CSSEdgeSizeViewportValues = CSSSizeValue<CSSViewportUnitValue>;
 type CSSEdgeSizeRespectValues = CSSSizeValue<CSSRespectUnitValue>;
 type CSSEdgeSizePercentageValues = CSSSizeValue<CSSPercentageUnitValue>;
-type CSSEdgeSizeValues =
+
+type CSSPaddingEdgeSizeValues =
   | CSSEdgeSizeAbsoluteValues
   | CSSEdgeSizeRelativeValues
   | CSSEdgeSizeViewportValues
   | CSSEdgeSizeRespectValues
   | CSSEdgeSizePercentageValues
+  | CSSEdgeSizeCalcExpressionFunctioin
+  | CSSGlobalValue
   | CSSVariableValue;
 
-type CustomRadiusValue<T extends CSSAbsoluteUnitValue | CSSRelativeUnitValue | CSSViewportUnitValue | CSSRespectUnitValue | CSSPercentageUnitValue> =
+type CustomRadiusValue<T extends CSSAbsoluteUnitValue | CSSLocalFontRelativeUnitValue | CSSViewportUnitValue | CSSRespectUnitValue | CSSPercentageUnitValue> =
   `${T} ${T} / ${T} ${T}`;
 
-type CSSRadiusValue<T extends CSSAbsoluteUnitValue | CSSRelativeUnitValue | CSSViewportUnitValue | CSSRespectUnitValue | CSSPercentageUnitValue> =
+type CSSRadiusValue<T extends CSSAbsoluteUnitValue | CSSLocalFontRelativeUnitValue | CSSViewportUnitValue | CSSRespectUnitValue | CSSPercentageUnitValue> =
   | CSSSizeValue<T>
   | CustomRadiusValue<T>;
 
 type CSSRadiusAbsoluteValues = CSSRadiusValue<CSSAbsoluteUnitValue>;
-type CSSRadiusRelativeValues = CSSRadiusValue<CSSRelativeUnitValue>;
+type CSSRadiusRelativeValues = CSSRadiusValue<CSSLocalFontRelativeUnitValue>;
 type CSSRadiusViewportValues = CSSRadiusValue<CSSViewportUnitValue>;
 type CSSRadiusRespectValues = CSSRadiusValue<CSSRespectUnitValue>;
 type CSSRadiusPercentageValues = CSSRadiusValue<CSSPercentageUnitValue>;
@@ -53,37 +93,39 @@ type CSSRadiusValues =
   | CSSVariableValue;
 
 export type CustomExtendProperties = {
-  width?: CSSNumericValue;
-  height?: CSSNumericValue;
-  margin?: CSSEdgeSizeValues;
-  marginBottom?: CSSNumericValue;
-  marginLeft?: CSSNumericValue;
-  marginRight?: CSSNumericValue;
-  marginTop?: CSSNumericValue;
-  padding?: CSSEdgeSizeValues;
+  width?: CSSNumericValue | CSSLengthSubValue;
+  height?: CSSNumericValue | CSSLengthSubValue;
+  margin?: CSSMarginEdgeSizeValues;
+  marginBottom?: CSSNumericValue | 'auto';
+  marginLeft?: CSSNumericValue | 'auto';
+  marginRight?: CSSNumericValue | 'auto';
+  marginTop?: CSSNumericValue | 'auto';
+  padding?: CSSPaddingEdgeSizeValues;
   paddingBottom?: CSSNumericValue;
   paddingLeft?: CSSNumericValue;
   paddingRight?: CSSNumericValue;
   paddingTop?: CSSNumericValue;
-  fontSize?: CSSNumericValue;
-  lineHeight?: CSSNumericValue;
-  letterSpacing?: CSSNumericValue;
-  wordSpacing?: CSSNumericValue;
-  borderWidth?: CSSNumericValue;
+  fontSize?: CSSNumericValue | CSSFontSizeSubValue;
+  lineHeight?: CSSNumericValue | 'normal';
+  letterSpacing?: CSSNumericValue | 'normal';
+  wordSpacing?: CSSNumericValue | 'normal';
+  borderWidth?: CSSNumericValue | 'thin' | 'medium' | 'thick';
   borderRadius?: CSSRadiusValues;
-  top?: CSSNumericValue;
-  right?: CSSNumericValue;
-  bottom?: CSSNumericValue;
-  left?: CSSNumericValue;
-  maxWidth?: CSSNumericValue;
-  maxHeight?: CSSNumericValue;
-  minWidth?: CSSNumericValue;
-  minHeight?: CSSNumericValue;
+  top?: CSSNumericValue | 'auto';
+  right?: CSSNumericValue | 'auto';
+  bottom?: CSSNumericValue | 'auto';
+  left?: CSSNumericValue | 'auto';
+  maxWidth?: CSSNumericValue | CSSLengthSubValue;
+  maxHeight?: CSSNumericValue | CSSLengthSubValue;
+  minWidth?: CSSNumericValue | CSSLengthSubValue;
+  minHeight?: CSSNumericValue | CSSLengthSubValue;
   flexBasis?: CSSNumericValue;
-  gap?: CSSNumericValue;
-  gridGap?: CSSNumericValue;
-  gridColumnGap?: CSSNumericValue;
-  gridRowGap?: CSSNumericValue;
+  gap?: CSSNumericValue | CSSCalcExpressionFunctioin;
+  rowGap?: CSSNumericValue;
+  columnGap?: CSSNumericValue | 'normal';
+  columns?: CSSColumnsValue;
+  gridColumn?: string;
+  gridRow?: string;
   color?: CSSColorValue;
   background?: CSSColorValue;
   backgroundColor?: CSSColorValue;
@@ -133,13 +175,12 @@ type CSSVariableValue = `var(${CSSVariableKey})`;
 type CSSColorValue = CSSColorNames | CSSVariableValue;
 
 export type CustomCSSProperties =
-  | (CustomExtendProperties & {
+  | CustomExtendProperties & {
       [K in keyof React.CSSProperties]: React.CSSProperties[K] | CSSVariableValue;
-    })
-  | ClassesObjectType;
+    };
 
 export type ClassesObjectType = {
-  [className in string]: CustomCSSProperties;
+  [className: string]: CustomCSSProperties;
 };
 
 export type ReturnStyleType<T> = { [key in keyof T]: string };
