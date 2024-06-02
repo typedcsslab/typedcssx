386723b26f4778692a20ddaa8ab58edf380b8d26
S.Watanabe <ocelot2828@gmail.com>
2024-06-02 12:21:22 +0900

    feat: Added assertions to align with type updates

--- src/core/media-query.ts
+++ src/core/media-query.ts
@@ -3,6 +3,6 @@ import type { CustomCSSProperties, ClassesObjectType } from '../_internal';
 export const media = (query: string, secondary?: string) => {
   return (target: CustomCSSProperties | ClassesObjectType): ClassesObjectType => {
     const mediaQuery = `@media (${query}${secondary ? ' and ' + secondary : ''})`;
-    return { [mediaQuery]: target };
+    return { [mediaQuery]: target } as ClassesObjectType;
   };
 };
