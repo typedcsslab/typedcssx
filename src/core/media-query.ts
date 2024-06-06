9a69c78961060b8b6bbd1355fd8ea7e398f7a52e
S.Watanabe <ocelot2828@gmail.com>
2024-06-06 15:07:39 +0900

    fix(media-query.ts): Fixed to return class names when wrapping selectors as wrappers

--- src/core/media-query.ts
+++ src/core/media-query.ts
@@ -1,8 +1,26 @@
-import type { CustomCSSProperties, ClassesObjectType } from '../_internal';
+import type { CustomCSSProperties, ClassesObjectType, ReturnStyleType } from '../_internal';
+import styles from '../core/styles/style.module.css';
+import { isClassesObjectType, isInDevelopment } from '../_internal';
 
 export const media = (query: string, secondary?: string) => {
-  return (target: CustomCSSProperties | ClassesObjectType): ClassesObjectType => {
+  return <T extends ClassesObjectType>(object: T | CustomCSSProperties): T => {
     const mediaQuery = `@media (${query}${secondary ? ' and ' + secondary : ''})`;
-    return { [mediaQuery]: target };
+
+    if (isClassesObjectType(object)) {
+      const result: ClassesObjectType = {};
+
+      Object.keys(object).forEach((className) => {
+        result[className] = {
+          ...((isInDevelopment ? className : styles[className]) as ReturnStyleType<T>),
+        };
+      });
+
+      return {
+        ...({ [mediaQuery]: object } as unknown as T),
+        ...(result as T),
+      };
+    } else {
+      return { [mediaQuery]: object } as T;
+    }
   };
 };
