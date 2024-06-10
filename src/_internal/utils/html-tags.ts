dea9767c209f924a87ad5f00a5344a5eb98af653
S.Watanabe <ocelot2828@gmail.com>
2024-06-10 12:22:24 +0900

    feat(html-tags and language-codes): Add types HtmlTags and LanguageCodes

---
+++ src/_internal/utils/html-tags.ts
@@ -0,0 +1,230 @@
+export type HtmlTags =
+  | 'A'
+  | 'Abbr'
+  | 'Address'
+  | 'Area'
+  | 'Article'
+  | 'Aside'
+  | 'Audio'
+  | 'B'
+  | 'Bdi'
+  | 'Bdo'
+  | 'Blockquote'
+  | 'Body'
+  | 'Br'
+  | 'Button'
+  | 'Canvas'
+  | 'Caption'
+  | 'Cite'
+  | 'Code'
+  | 'Col'
+  | 'Colgroup'
+  | 'Data'
+  | 'Datalist'
+  | 'Dd'
+  | 'Del'
+  | 'Details'
+  | 'Dfn'
+  | 'Dialog'
+  | 'Div'
+  | 'Dl'
+  | 'Dt'
+  | 'Em'
+  | 'Embed'
+  | 'Fieldset'
+  | 'Figcaption'
+  | 'Figure'
+  | 'Footer'
+  | 'Form'
+  | 'H1'
+  | 'H2'
+  | 'H3'
+  | 'H4'
+  | 'H5'
+  | 'H6'
+  | 'Header'
+  | 'Hgroup'
+  | 'Hr'
+  | 'Html'
+  | 'I'
+  | 'Iframe'
+  | 'Img'
+  | 'Input'
+  | 'Ins'
+  | 'Kbd'
+  | 'Label'
+  | 'Legend'
+  | 'Li'
+  | 'Main'
+  | 'Map'
+  | 'Mark'
+  | 'Math'
+  | 'Menu'
+  | 'Menuitem'
+  | 'Meter'
+  | 'Nav'
+  | 'Noscript'
+  | 'Object'
+  | 'Ol'
+  | 'Optgroup'
+  | 'Option'
+  | 'Output'
+  | 'P'
+  | 'Param'
+  | 'Picture'
+  | 'Pre'
+  | 'Progress'
+  | 'Q'
+  | 'Rb'
+  | 'Rp'
+  | 'Rt'
+  | 'Rtc'
+  | 'Ruby'
+  | 'S'
+  | 'Samp'
+  | 'Script'
+  | 'Search'
+  | 'Section'
+  | 'Select'
+  | 'Slot'
+  | 'Small'
+  | 'Source'
+  | 'Span'
+  | 'Strong'
+  | 'Sub'
+  | 'Summary'
+  | 'Sup'
+  | 'Svg'
+  | 'Table'
+  | 'Tbody'
+  | 'Td'
+  | 'Template'
+  | 'Textarea'
+  | 'Tfoot'
+  | 'Th'
+  | 'Thead'
+  | 'Time'
+  | 'Tr'
+  | 'Track'
+  | 'U'
+  | 'Ul'
+  | 'Var'
+  | 'Video'
+  | 'Wbr';
+
+const htmlTags = [
+  'a',
+  'abbr',
+  'address',
+  'area',
+  'article',
+  'aside',
+  'audio',
+  'b',
+  'bdi',
+  'bdo',
+  'blockquote',
+  'body',
+  'br',
+  'button',
+  'canvas',
+  'caption',
+  'cite',
+  'code',
+  'col',
+  'colgroup',
+  'data',
+  'datalist',
+  'dd',
+  'del',
+  'details',
+  'dfn',
+  'dialog',
+  'div',
+  'dl',
+  'dt',
+  'em',
+  'embed',
+  'fieldset',
+  'figcaption',
+  'figure',
+  'footer',
+  'form',
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'header',
+  'hgroup',
+  'hr',
+  'html',
+  'i',
+  'iframe',
+  'img',
+  'input',
+  'ins',
+  'kbd',
+  'label',
+  'legend',
+  'li',
+  'main',
+  'map',
+  'mark',
+  'math',
+  'menu',
+  'menuitem',
+  'meter',
+  'nav',
+  'noscript',
+  'object',
+  'ol',
+  'optgroup',
+  'option',
+  'output',
+  'p',
+  'param',
+  'picture',
+  'pre',
+  'progress',
+  'q',
+  'rb',
+  'rp',
+  'rt',
+  'rtc',
+  'ruby',
+  's',
+  'samp',
+  'script',
+  'search',
+  'section',
+  'select',
+  'slot',
+  'small',
+  'source',
+  'span',
+  'strong',
+  'sub',
+  'summary',
+  'sup',
+  'svg',
+  'table',
+  'tbody',
+  'td',
+  'template',
+  'textarea',
+  'tfoot',
+  'th',
+  'thead',
+  'time',
+  'tr',
+  'track',
+  'u',
+  'ul',
+  'var',
+  'video',
+  'wbr',
+];
+
+export default htmlTags;
---
+++ src/_internal/utils/language-codes.ts
@@ -0,0 +1,372 @@
+export type LanguageCodes =
+  | 'Aa'
+  | 'Ab'
+  | 'Ae'
+  | 'Af'
+  | 'Ak'
+  | 'Am'
+  | 'An'
+  | 'Ar'
+  | 'As'
+  | 'Av'
+  | 'Ay'
+  | 'Az'
+  | 'Ba'
+  | 'Be'
+  | 'Bg'
+  | 'Bi'
+  | 'Bm'
+  | 'Bn'
+  | 'Bo'
+  | 'Br'
+  | 'Bs'
+  | 'Ca'
+  | 'Ce'
+  | 'Ch'
+  | 'Co'
+  | 'Cr'
+  | 'Cs'
+  | 'Cu'
+  | 'Cv'
+  | 'Cy'
+  | 'Da'
+  | 'De'
+  | 'Dv'
+  | 'Dz'
+  | 'Ee'
+  | 'El'
+  | 'En'
+  | 'Eo'
+  | 'Es'
+  | 'Et'
+  | 'Eu'
+  | 'Fa'
+  | 'Ff'
+  | 'Fi'
+  | 'Fj'
+  | 'Fo'
+  | 'Fr'
+  | 'Fy'
+  | 'Ga'
+  | 'Gd'
+  | 'Gl'
+  | 'Gn'
+  | 'Gu'
+  | 'Gv'
+  | 'Ha'
+  | 'He'
+  | 'Hi'
+  | 'Ho'
+  | 'Hr'
+  | 'Ht'
+  | 'Hu'
+  | 'Hy'
+  | 'Hz'
+  | 'Ia'
+  | 'Id'
+  | 'Ie'
+  | 'Ig'
+  | 'Ii'
+  | 'Ik'
+  | 'Io'
+  | 'Is'
+  | 'It'
+  | 'Iu'
+  | 'Ja'
+  | 'Jv'
+  | 'Ka'
+  | 'Kg'
+  | 'Ki'
+  | 'Kj'
+  | 'Kk'
+  | 'Kl'
+  | 'Km'
+  | 'Kn'
+  | 'Ko'
+  | 'Kr'
+  | 'Ks'
+  | 'Ku'
+  | 'Kv'
+  | 'Kw'
+  | 'Ky'
+  | 'La'
+  | 'Lb'
+  | 'Lg'
+  | 'Li'
+  | 'Ln'
+  | 'Lo'
+  | 'Lt'
+  | 'Lu'
+  | 'Lv'
+  | 'Mg'
+  | 'Mh'
+  | 'Mi'
+  | 'Mk'
+  | 'Ml'
+  | 'Mn'
+  | 'Mr'
+  | 'Ms'
+  | 'Mt'
+  | 'My'
+  | 'Na'
+  | 'Nb'
+  | 'Nd'
+  | 'Ne'
+  | 'Ng'
+  | 'Nl'
+  | 'Nn'
+  | 'No'
+  | 'Nr'
+  | 'Nv'
+  | 'Ny'
+  | 'Oc'
+  | 'Oj'
+  | 'Om'
+  | 'Or'
+  | 'Os'
+  | 'Pa'
+  | 'Pi'
+  | 'Pl'
+  | 'Ps'
+  | 'Pt'
+  | 'Qu'
+  | 'Rm'
+  | 'Rn'
+  | 'Ro'
+  | 'Ru'
+  | 'Rw'
+  | 'Sa'
+  | 'Sc'
+  | 'Sd'
+  | 'Se'
+  | 'Sg'
+  | 'Si'
+  | 'Sk'
+  | 'Sl'
+  | 'Sm'
+  | 'Sn'
+  | 'So'
+  | 'Sq'
+  | 'Sr'
+  | 'Ss'
+  | 'St'
+  | 'Su'
+  | 'Sv'
+  | 'Sw'
+  | 'Ta'
+  | 'Te'
+  | 'Tg'
+  | 'Th'
+  | 'Ti'
+  | 'Tk'
+  | 'Tl'
+  | 'Tn'
+  | 'To'
+  | 'Tr'
+  | 'Ts'
+  | 'Tt'
+  | 'Tw'
+  | 'Ty'
+  | 'Ug'
+  | 'Uk'
+  | 'Ur'
+  | 'Uz'
+  | 'Ve'
+  | 'Vi'
+  | 'Vo'
+  | 'Wa'
+  | 'Wo'
+  | 'Xh'
+  | 'Yi'
+  | 'Yo'
+  | 'Za'
+  | 'Zh'
+  | 'Zu';
+
+const languageCodes = [
+  'aa',
+  'ab',
+  'ae',
+  'af',
+  'ak',
+  'am',
+  'an',
+  'ar',
+  'as',
+  'av',
+  'ay',
+  'az',
+  'ba',
+  'be',
+  'bg',
+  'bi',
+  'bm',
+  'bn',
+  'bo',
+  'br',
+  'bs',
+  'ca',
+  'ce',
+  'ch',
+  'co',
+  'cr',
+  'cs',
+  'cu',
+  'cv',
+  'cy',
+  'da',
+  'de',
+  'dv',
+  'dz',
+  'ee',
+  'el',
+  'en',
+  'eo',
+  'es',
+  'et',
+  'eu',
+  'fa',
+  'ff',
+  'fi',
+  'fj',
+  'fo',
+  'fr',
+  'fy',
+  'ga',
+  'gd',
+  'gl',
+  'gn',
+  'gu',
+  'gv',
+  'ha',
+  'he',
+  'hi',
+  'ho',
+  'hr',
+  'ht',
+  'hu',
+  'hy',
+  'hz',
+  'ia',
+  'id',
+  'ie',
+  'ig',
+  'ii',
+  'ik',
+  'io',
+  'is',
+  'it',
+  'iu',
+  'ja',
+  'jv',
+  'ka',
+  'kg',
+  'ki',
+  'kj',
+  'kk',
+  'kl',
+  'km',
+  'kn',
+  'ko',
+  'kr',
+  'ks',
+  'ku',
+  'kv',
+  'kw',
+  'ky',
+  'la',
+  'lb',
+  'lg',
+  'li',
+  'ln',
+  'lo',
+  'lt',
+  'lu',
+  'lv',
+  'mg',
+  'mh',
+  'mi',
+  'mk',
+  'ml',
+  'mn',
+  'mr',
+  'ms',
+  'mt',
+  'my',
+  'na',
+  'nb',
+  'nd',
+  'ne',
+  'ng',
+  'nl',
+  'nn',
+  'no',
+  'nr',
+  'nv',
+  'ny',
+  'oc',
+  'oj',
+  'om',
+  'or',
+  'os',
+  'pa',
+  'pi',
+  'pl',
+  'ps',
+  'pt',
+  'qu',
+  'rm',
+  'rn',
+  'ro',
+  'ru',
+  'rw',
+  'sa',
+  'sc',
+  'sd',
+  'se',
+  'sg',
+  'si',
+  'sk',
+  'sl',
+  'sm',
+  'sn',
+  'so',
+  'sq',
+  'sr',
+  'ss',
+  'st',
+  'su',
+  'sv',
+  'sw',
+  'ta',
+  'te',
+  'tg',
+  'th',
+  'ti',
+  'tk',
+  'tl',
+  'tn',
+  'to',
+  'tr',
+  'ts',
+  'tt',
+  'tw',
+  'ty',
+  'ug',
+  'uk',
+  'ur',
+  'uz',
+  've',
+  'vi',
+  'vo',
+  'wa',
+  'wo',
+  'xh',
+  'yi',
+  'yo',
+  'za',
+  'zh',
+  'zu',
+];
+
+export default languageCodes;
