3cfa57583bdaef7739b328d41736b94efed42e2d
S.Watanabe <ocelot2828@gmail.com>
2024-06-12 15:29:24 +0900

    fix(media-query.ts): revert not return className

--- src/core/media-query.ts
+++ src/core/media-query.ts
@@ -1,26 +1,9 @@
-import type { CustomCSSProperties, ClassesObjectType, ReturnStyleType, ExactClassesObjectType } from '../_internal';
-import styles from '../core/styles/style.module.css';
-import { isClassesObjectType, isInDevelopment } from '../_internal';
+import type { CustomCSSProperties, ClassesObjectType, ExactClassesObjectType } from '../_internal';
 
 export const media = (query: string, secondary?: string) => {
   return <T extends ClassesObjectType>(object: ExactClassesObjectType<T> | ClassesObjectType | CustomCSSProperties): ClassesObjectType => {
     const mediaQuery = `@media (${query}${secondary ? ' and ' + secondary : ''})`;
 
-    if (isClassesObjectType(object)) {
-      const result: ClassesObjectType = {};
-
-      Object.keys(object).forEach((className) => {
-        result[className] = {
-          ...((isInDevelopment ? className : styles[className]) as ReturnStyleType<T>),
-        };
-      });
-
-      return {
-        ...{ [mediaQuery]: object },
-        ...result,
-      };
-    } else {
-      return { [mediaQuery]: object };
-    }
+    return { [mediaQuery]: object };
   };
 };
