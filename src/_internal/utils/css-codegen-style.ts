6b08399807b50b42be04831377d36e24222af665
S.Watanabe <ocelot2828@gmail.com>
2024-06-25 19:51:35 +0900

    fix(css-codegen-style.ts): Added support for has and not other refactor exception

--- src/_internal/utils/css-codegen-style.ts
+++ src/_internal/utils/css-codegen-style.ts
@@ -1,4 +1,4 @@
-import { camelToKebabCase, pseudo } from '..';
+import { camelToKebabCase, exception, pseudo } from '..';
 import type { CustomCSSProperties, PropertyType } from '..';
 
 export function cssCodeGenStyle<T extends CustomCSSProperties>(object: T, base62Hash?: string, root?: string) {
@@ -18,9 +18,20 @@ export function cssCodeGenStyle<T extends CustomCSSProperties>(object: T, base62
     for (const property in properties) {
       const value = (properties as unknown as PropertyType)[property];
 
-      if (pseudo.classes.includes(property) || pseudo.elements.includes(property) || property.includes('&')) {
-        const CSSProp = camelToKebabCase(property.replace('&', ''));
-        const pseudoSelector = property.includes('&') ? `${CSSProp}` : pseudo.classes.includes(property) ? `:${CSSProp}` : `::${CSSProp}`;
+      if (
+        pseudo.classes.includes(property) ||
+        pseudo.elements.includes(property) ||
+        property.includes('&') ||
+        property.startsWith('has') ||
+        property.startsWith('not')
+      ) {
+        let CSSProp = camelToKebabCase(property.replace('&', ''));
+        if (property.startsWith('has') || property.startsWith('not') || property.includes('&')) {
+          CSSProp = camelToKebabCase(property);
+        } else {
+          CSSProp = pseudo.classes.includes(property) ? `:${CSSProp}` : `::${CSSProp}`;
+        }
+        const pseudoSelector = property.includes('&') ? `${CSSProp}` : CSSProp;
         const pseudoRuleSet = stringConverter(className + pseudoSelector, value as never, indentLevel + 1);
         pseudoRules += `${indent}${className}${pseudoSelector} {\n${pseudoRuleSet.mainRules}${pseudoRuleSet.pseudoRules}${indent}}\n`;
         mediaQueries += pseudoRuleSet.media;
@@ -32,12 +43,7 @@ export function cssCodeGenStyle<T extends CustomCSSProperties>(object: T, base62
         bigIndent = false;
       } else if (typeof value === 'string' || typeof value === 'number') {
         const CSSProp = camelToKebabCase(property);
-        const applyValue =
-          typeof value === 'number' && (CSSProp === 'line-height' || CSSProp === 'opacity' || CSSProp === 'scale' || CSSProp === 'font-weight')
-            ? value
-            : typeof value === 'number'
-            ? value + 'px'
-            : value;
+        const applyValue = typeof value === 'number' && exception.includes(CSSProp) ? value : typeof value === 'number' ? value + 'px' : value;
         mainRules += `${bigIndent ? '    ' : '  '}${CSSProp}: ${applyValue};\n`;
       }
     }
