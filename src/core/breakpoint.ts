d9fd2a83afd742878ecacc05279d2283f7a5520e
S.Watanabe <ocelot2828@gmail.com>
2024-07-27 21:14:38 +0900

    feat(media query): The media function has been deprecated and replaced with a method for writing queries directly.

---
+++ src/core/breakpoint.ts
@@ -0,0 +1,11 @@
+export const min_sm = '@media screen and (min-width: 640px)';
+export const min_md = '@media screen and (min-width: 768px)';
+export const min_lg = '@media screen and (min-width: 1024px)';
+export const min_xl = '@media screen and (min-width: 1280px)';
+export const min_2xl = '@media screen and (min-width: 1536px)';
+
+export const max_sm = '@media screen and (max-width: 640px)';
+export const max_md = '@media screen and (max-width: 768px)';
+export const max_lg = '@media screen and (max-width: 1024px)';
+export const max_xl = '@media screen and (max-width: 1280px)';
+export const max_2xl = '@media screen and (max-width: 1536px)';
--- src/core/media-query.ts
+++
@@ -1,9 +0,0 @@
-import type { CustomCSSProperties, ClassesObjectType, ExactClassesObjectType } from '../_internal';
-
-export const media = (query: string, secondary?: string) => {
-  return <T extends ClassesObjectType>(object: ExactClassesObjectType<T> | CustomCSSProperties): ClassesObjectType => {
-    const mediaQuery = `@media (${query}${secondary ? ' and ' + secondary : ''})`;
-
-    return { [mediaQuery]: object };
-  };
-};
