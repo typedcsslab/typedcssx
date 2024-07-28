72bbd3b0d51b060644735a4765b65087cc1f90f6
S.Watanabe <ocelot2828@gmail.com>
2024-07-28 10:00:50 +0900

    fix(breakpoints): Removed screen and

--- src/core/breakpoint.ts
+++ src/core/breakpoint.ts
@@ -1,11 +1,11 @@
-export const min_sm = '@media screen and (min-width: 640px)';
-export const min_md = '@media screen and (min-width: 768px)';
-export const min_lg = '@media screen and (min-width: 1024px)';
-export const min_xl = '@media screen and (min-width: 1280px)';
-export const min_2xl = '@media screen and (min-width: 1536px)';
+export const min_sm = '@media (min-width: 640px)';
+export const min_md = '@media (min-width: 768px)';
+export const min_lg = '@media (min-width: 1024px)';
+export const min_xl = '@media (min-width: 1280px)';
+export const min_2xl = '@media (min-width: 1536px)';
 
-export const max_sm = '@media screen and (max-width: 640px)';
-export const max_md = '@media screen and (max-width: 768px)';
-export const max_lg = '@media screen and (max-width: 1024px)';
-export const max_xl = '@media screen and (max-width: 1280px)';
-export const max_2xl = '@media screen and (max-width: 1536px)';
+export const max_sm = '@media (max-width: 640px)';
+export const max_md = '@media (max-width: 768px)';
+export const max_lg = '@media (max-width: 1024px)';
+export const max_xl = '@media (max-width: 1280px)';
+export const max_2xl = '@media (max-width: 1536px)';
