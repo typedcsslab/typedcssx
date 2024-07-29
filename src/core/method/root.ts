82398c32250eca13484cac381d944e0de3239eff
S.Watanabe <ocelot2828@gmail.com>
2024-07-29 17:03:27 +0900

    fix(root.ts): There was an omission in the type definition and it was corrected.

--- src/core/method/root.ts
+++ src/core/method/root.ts
@@ -1,8 +1,8 @@
-import type { CustomCSSProperties } from '../../_internal';
+import type { ExtendedCSSProperties } from '../../_internal';
 import { isInDevelopment, injectCSSGlobal, styleCompiler } from '../../_internal';
 import { resolveGlobalStyleSheet, globalStyleSheetPromise, createGlobalStyleSheetPromise } from './root-build-in-helper';
 
-export function root(object: CustomCSSProperties): void {
+export function root(object: ExtendedCSSProperties): void {
   const { styleSheet } = styleCompiler(object, undefined, '--root');
   if (typeof globalStyleSheetPromise === 'undefined') createGlobalStyleSheetPromise();
   resolveGlobalStyleSheet([styleSheet, '--global']);
