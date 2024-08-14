42db7637e0d668bf5cda5f09bc259f86d091787c
S.Watanabe <ocelot2828@gmail.com>
2024-08-14 10:10:39 +0900

    refactor(e2e/test*): fix variable name and test name

--- e2e/test/class-conflict.test.ts
+++ e2e/test/class-conflict.test.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Class conflict resolution works correctly', async ({ page }) => {
   await page.goto('http://localhost:4000');
   await page.waitForSelector('[data-testid="e2e-test-span"]');
-  const div = page.locator('[data-testid="e2e-test-span"]');
-  await div.evaluate((node) => node.classList.add('test_conflict'));
-  await expect(div).toHaveCSS('font-size', '14px');
+  const span = page.locator('[data-testid="e2e-test-span"]');
+  await span.evaluate((node) => node.classList.add('test_conflict'));
+  await expect(span).toHaveCSS('font-size', '14px');
 });
