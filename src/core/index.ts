4f06bef10ef906f6108f4a3d94c9187932003726
S.Watanabe <ocelot2828@gmail.com>
2024-08-17 17:17:56 +0900

    feat(prefers-color-scheme): add

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
 export * from './breakpoint';
+export * from './prefers-color-scheme';
 import Style from './style';
 export default Style;
---
+++ src/core/prefers-color-scheme.ts
@@ -0,0 +1,2 @@
+export const dark = '@media (prefers-color-scheme: dark)';
+export const light = '@media (prefers-color-scheme: light)';
