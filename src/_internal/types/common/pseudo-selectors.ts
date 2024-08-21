00402240c75e2ae458ba8881bb4569d7fc875d6a
S.Watanabe <ocelot2828@gmail.com>
2024-08-21 20:38:04 +0900

    perf(pseudo-selectors.ts): Types, including all Class, have been removed.

--- src/_internal/types/common/pseudo-selectors.ts
+++ src/_internal/types/common/pseudo-selectors.ts
@@ -13,11 +13,6 @@ type NotType = {
   [key in Not]?: CustomCSSProperties;
 };
 
-type NotClass<T extends string> = `notClass${Capitalize<T>}${string}`;
-type NotClassType = {
-  [key in NotClass<string>]?: CustomCSSProperties;
-};
-
 type Has = `has${HtmlTags}`;
 type HasType = {
   [key in Has]?: CustomCSSProperties;
@@ -33,56 +28,17 @@ type HasPlusType = {
   [key in HasPlus]?: CustomCSSProperties;
 };
 
-export type HasEPE = `has${HtmlTags}Plus${HtmlTags | string}`;
+export type HasEPE = `has${HtmlTags | string}Plus${HtmlTags | string}`;
 type HasEPEType = {
   [key in HasEPE]?: CustomCSSProperties;
 };
 
-export type HasECE = `has${HtmlTags}Child${HtmlTags | string}`;
+export type HasECE = `has${HtmlTags | string}Child${HtmlTags | string}`;
 type HasECEType = {
   [key in HasECE]?: CustomCSSProperties;
 };
 
-type HasClass<T extends string> = `hasClass${Capitalize<T>}${string}`;
-type HasClassType = {
-  [key in HasClass<string>]?: CustomCSSProperties;
-};
-
-type HasClassChild<T extends string> = `hasClassChild${Capitalize<T>}${string}`;
-type HasClassChildType = {
-  [key in HasClassChild<string>]?: CustomCSSProperties;
-};
-
-type HasClassPlus<T extends string> = `hasClassPlus${Capitalize<T>}${string}`;
-type HasClassPlusType = {
-  [key in HasClassPlus<string>]?: CustomCSSProperties;
-};
-
-export type HasCPC = `hasClass${string}Plus${string | HtmlTags}`;
-type HasCPCType = {
-  [key in HasCPC]?: CustomCSSProperties;
-};
-
-export type HasCCC = `hasClass${string}Child${string | HtmlTags}`;
-type HasCCCType = {
-  [key in HasCCC]?: CustomCSSProperties;
-};
-
-export type ArgsPseudos =
-  | HasCCCType
-  | HasCPCType
-  | HasClassPlusType
-  | HasClassChildType
-  | HasClassType
-  | HasECEType
-  | HasEPEType
-  | HasPlusType
-  | HasChildType
-  | HasType
-  | NotClassType
-  | NotType
-  | LanguagesType
-  | CustomNthArgsType;
+export type ArgsPseudos = HasECEType | HasEPEType | HasPlusType | HasChildType | HasType | NotType | LanguagesType | CustomNthArgsType;
 
 export type AndStrings = `&${string}`;
 export type AndStringsType = {
