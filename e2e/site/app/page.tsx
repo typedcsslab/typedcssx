cc84f20235d7ee5a0ba3a0eaa56cf9475a147fff
S.Watanabe <ocelot2828@gmail.com>
2024-08-22 20:06:41 +0900

    feat(app/page): add Link for server page

--- e2e/site/app/page.tsx
+++ e2e/site/app/page.tsx
@@ -1,9 +1,10 @@
 import { E2ETest } from '@/component/style-create.css';
 import { Conflict } from '@/component/style-not-conflict.css';
+import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      Typed CSS X E2E Test
+      Typed CSS X E2E Test <Link href="/server">Server Page</Link>
       <E2ETest />
       <Conflict />
     </main>
