79f312bb79cb18ca09d030887939b4c673de2f11
S.Watanabe <ocelot2828@gmail.com>
2024-08-22 20:04:54 +0900

    feat(ServerComponent*): add

---
+++ e2e/site/app/server/page.tsx
@@ -0,0 +1,9 @@
+import { ServerComponent } from '@/component/style-server-component.css';
+
+export default function Page() {
+  return (
+    <main>
+      <ServerComponent />
+    </main>
+  );
+}
---
+++ e2e/site/component/style-server-component.css.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Style from 'typedcssx';
+
+const styles = Style.create({
+  test_server: {
+    color: 'green',
+  },
+});
+
+export const ServerComponent = () => {
+  return (
+    <p className={styles.test_server} data-testid="e2e-test-p">
+      ServerComponent
+    </p>
+  );
+};
