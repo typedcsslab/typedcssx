9c00738ea4cf45be6152a9ec194871d747a7bb2b
S.Watanabe <ocelot2828@gmail.com>
2024-08-23 15:24:59 +0900

    fix(_internal/index): remove import PreviewServerCSS.

--- src/_internal/index.ts
+++ src/_internal/index.ts
@@ -5,7 +5,6 @@ export { genBase62Hash } from './utils/hash';
 export { injectClientCSS } from './utils/inject-client-css';
 export { injectClientGlobalCSS } from './utils/inject-client-global-css';
 export { injectServerCSS, getServerCSS } from './utils/inject-server-css';
-export { PreviewServerCSS } from './utils/server-css';
 export { buildIn } from './utils/build-in';
 export { sheetCompiler } from './utils/sheet-compiler';
 export { styleCompiler } from './utils/style-compiler';
