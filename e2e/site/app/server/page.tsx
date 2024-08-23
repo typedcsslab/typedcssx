f3e7947ae29e820c4c1113ba1c2522d81bd6eaab
S.Watanabe <ocelot2828@gmail.com>
2024-08-23 15:26:29 +0900

    feat(app/server/page): Add a Link for anchorEnabled check.

--- e2e/site/app/server/page.tsx
+++ e2e/site/app/server/page.tsx
@@ -1,9 +1,11 @@
 import { ServerComponent } from '@/component/style-server-component.css';
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <main>
       <ServerComponent />
+      <Link href="/">Server Page</Link>
     </main>
   );
 }
