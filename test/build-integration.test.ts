4daf0a3a0ad1137ce70a196bc22bba38e0662e19
S.Watanabe <ocelot2828@gmail.com>
2024-09-02 11:19:48 +0900

    test(build-integration.test.ts): add

---
+++ test/build-integration.test.ts
@@ -0,0 +1,15 @@
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import path from 'path';
+
+const execPromise = promisify(exec);
+
+test('Should be build successfully and logs contain expected output', async () => {
+  const { stdout } = await execPromise('npm run build', {
+    cwd: path.join(__dirname, '../e2e/site'),
+  });
+
+  console.log('stdout:', stdout);
+  expect(stdout).toContain('Compiled successfully');
+  expect(stdout).toContain('(Static)  prerendered as static content');
+}, 20000);
