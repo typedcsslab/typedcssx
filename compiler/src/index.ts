048a8905dcc4e9d428418fad99b738d6e2baee8c
S.Watanabe <ocelot2828@gmail.com>
2024-09-12 21:11:11 +0900

    perf(compiler/index.ts): It is no longer necessary to specify css in the file extension.

--- compiler/src/index.ts
+++ compiler/src/index.ts
@@ -1,12 +1,35 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import * as ts from 'typescript';
 import { cleanUp } from './clean-up';
 import { createBuildIn } from '../../src/core/method/create-build-in-helper';
 import { setBuildIn } from '../../src/core/method/set-build-in-helper';
 import { globalBuildIn } from '../../src/core/method/global-build-in-helper';
 import { rootBuildIn } from '../../src/core/method/root-build-in-helper';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const fg = require('fast-glob');
+
+function isStyleClass(filePath: string): boolean {
+  const content = fs.readFileSync(filePath, 'utf8');
+  const sourceFile = ts.createSourceFile(filePath, content, ts.ScriptTarget.Latest, true);
+
+  let isUsed = false;
+
+  function checkNode(node: ts.Node) {
+    if (ts.isPropertyAccessExpression(node) && ts.isIdentifier(node.name)) {
+      const expressionText = node.expression.getText(sourceFile);
+      const methodName = node.name.getText(sourceFile);
+      const methods = ['create', 'set', 'root', 'global'];
+      if (expressionText === 'cssx' && methods.includes(methodName)) {
+        isUsed = true;
+      }
+    }
+
+    ts.forEachChild(node, checkNode);
+  }
+
+  checkNode(sourceFile);
+
+  return isUsed;
+}
 
 (async () => {
   await cleanUp();
@@ -19,10 +42,13 @@ const fg = require('fast-glob');
   } else {
     appRoot = path.join(process.cwd(), '../../');
   }
-  const csstsPattern = [path.join(appRoot, '**/*.css.ts'), path.join(appRoot, '**/*.css.tsx')];
-  const files = await fg(csstsPattern);
+  const csstsPattern = [path.join(appRoot, '**/*.ts'), path.join(appRoot, '**/*.tsx')];
+  const files = fs.globSync(csstsPattern);
+  const styleFiles = files.filter(file => {
+    return isStyleClass(file);
+  });
   console.log('\n💬 The following CSS caches were accepted:\n');
-  for (const file of files) {
+  for (const file of styleFiles) {
     const filePath = path.resolve(file);
     await import(filePath);
     createBuildIn();
