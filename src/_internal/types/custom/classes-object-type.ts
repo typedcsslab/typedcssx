5b1b6925097644bfb723c15f8989f4afbf0f4196
S.Watanabe <ocelot2828@gmail.com>
2024-09-15 11:53:55 +0900

    refactor(*): ReturnStyleType has been moved here because it is only used in the create function.

--- src/_internal/types/custom/classes-object-type.ts
+++ src/_internal/types/custom/classes-object-type.ts
@@ -12,3 +12,5 @@ type Exact<T, U> = T extends U ? T : never;
 export type ExactClassesObjectType<T> = {
   [K in keyof T | string]: K extends keyof T ? Exact<T[K], CustomCSSProperties> : CustomCSSProperties;
 };
+
+export type ReturnStyleType<T> = { [key in keyof T]: string };
