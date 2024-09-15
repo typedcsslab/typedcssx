d52397f6def416796f0e462f5947683ebf1107a7
S.Watanabe <ocelot2828@gmail.com>
2024-09-15 16:09:00 +0900

    fix(media.is): unnecessary line removed

--- src/core/media.ts
+++ src/core/media.ts
@@ -3,12 +3,10 @@ export const min_md = '@media (min-width: 768px)';
 export const min_lg = '@media (min-width: 1024px)';
 export const min_xl = '@media (min-width: 1280px)';
 export const min_2xl = '@media (min-width: 1536px)';
-
 export const max_sm = '@media (max-width: 640px)';
 export const max_md = '@media (max-width: 768px)';
 export const max_lg = '@media (max-width: 1024px)';
 export const max_xl = '@media (max-width: 1280px)';
 export const max_2xl = '@media (max-width: 1536px)';
-
 export const dark = '@media (prefers-color-scheme: dark)';
 export const light = '@media (prefers-color-scheme: light)';
