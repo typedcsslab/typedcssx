7b00e50882d2d4582acd3401df120c0bae2a842a
S.Watanabe <ocelot2828@gmail.com>
2024-09-17 19:05:08 +0900

    feat(e2e/site): add next-plugin

--- e2e/site/next.config.js
+++ e2e/site/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const { TypedCSSXNextPlugin, configCSSModule } = require('typedcssx-next-plugin');
+const nextConfig = {
+  webpack: config => {
+    config.plugins.push(new TypedCSSXNextPlugin());
+    return configCSSModule(config);
+  },
+};
 
 module.exports = nextConfig;
