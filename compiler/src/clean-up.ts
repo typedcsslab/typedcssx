4292769f654854b92da323705cb83e6a3e5ef2c0
S.Watanabe <ocelot2828@gmail.com>
2024-09-17 19:04:34 +0900

    fix(*): removed find-project-root func it was freaky

--- compiler/src/clean-up.ts
+++ compiler/src/clean-up.ts
@@ -1,33 +1,14 @@
-import { findProjectRoot } from '../../lib/find-project-root.mjs';
-import { existsSync, writeFileSync } from 'fs';
+import { writeFileSync } from 'fs';
 import { join } from 'path';
 
 export const cleanUp = async () => {
-  const projectRoot = await findProjectRoot(__dirname);
-
-  if (!projectRoot) {
-    console.error('Next.js project root not found');
-    return;
-  }
-
-  const srcDir = join(projectRoot, 'src');
-  let stylesDir: string;
-
-  if (existsSync(srcDir)) {
-    stylesDir = join(srcDir, 'styles');
-  } else {
-    stylesDir = join(projectRoot, 'styles');
-  }
-
   const styleFilePath = join(__dirname, '../../src/core/styles/style.module.css');
-  const globalFilePath = join(stylesDir, 'typedcssx-global.css');
+  const globalFilePath = join(__dirname, '../../src/core/styles/style.module.css');
 
   try {
     writeFileSync(styleFilePath, '/*______________________________*/', 'utf-8');
     console.log('...💫(reseted module css)');
-    if (existsSync(stylesDir)) {
-      writeFileSync(globalFilePath, '/*______________________________*/', 'utf-8');
-    }
+    writeFileSync(globalFilePath, '/*______________________________*/', 'utf-8');
     console.log('...💫(reseted global css)');
   } catch (err) {
     console.error('An error occurred:', err);
--- src/_internal/utils/build-in.ts
+++ src/_internal/utils/build-in.ts
@@ -1,68 +1,30 @@
 'use server';
 
-import { readFileSync, appendFileSync, mkdirSync, existsSync } from 'fs';
+import { readFileSync, appendFileSync, existsSync } from 'fs';
 import { isServer } from './helper';
-import { join, dirname } from 'path';
-import * as fs from 'fs';
-
-// Those affected by builds that exist in dist
-// Functions required on the outside and inside
-async function findProjectRoot(startPath: string): Promise<string | null> {
-  if (!isServer) return null;
-  let currentPath = startPath;
-  while (currentPath !== '/') {
-    const packageJsonPath = join(currentPath, 'package.json');
-    const lockFiles = ['pnpm-lock.yaml', 'package-lock.json', 'yarn.lock', 'bun.lockb'].some(file => fs.existsSync(join(currentPath, file)));
-    if (fs.existsSync(packageJsonPath) && lockFiles) {
-      return currentPath;
-    }
-    currentPath = dirname(currentPath);
-  }
-  return null;
-}
+import { join } from 'path';
 
 export const buildIn = async (styleSheet: string, global?: string): Promise<void> => {
-  const projectRoot = await findProjectRoot(__dirname);
-
-  if (!projectRoot) {
-    console.error('Next.js project root not found');
-    return;
-  }
-
-  const srcDir = join(projectRoot, 'src');
-  let stylesDir: string;
-
-  if (existsSync(srcDir)) {
-    stylesDir = join(srcDir, 'styles');
-  } else {
-    stylesDir = join(projectRoot, 'styles');
-  }
-
   const styleFilePath = join(__dirname, '../../core/styles/style.module.css');
-  const globalFilePath = join(stylesDir, 'typedcssx-global.css');
+  const globalFilePath = join(__dirname, '../../core/styles/global.css');
 
   const filePath = global === '--global' ? globalFilePath : styleFilePath;
   const message = global === '--global' ? ' ✅ Generating global static css \n' : ' ✅ Generating module static css \n';
 
-  if (!existsSync(stylesDir)) {
-    mkdirSync(stylesDir);
-  }
-
-  if (isServer) {
-    try {
-      if (existsSync(filePath)) {
-        const cssData = readFileSync(filePath, 'utf-8');
-        if (!cssData.includes(styleSheet)) {
-          appendFileSync(filePath, styleSheet, 'utf-8');
-          console.log(message + styleSheet);
-        }
-      } else {
+  if (!isServer) return;
+  try {
+    if (existsSync(filePath)) {
+      const cssData = readFileSync(filePath, 'utf-8');
+      if (!cssData.includes(styleSheet)) {
         appendFileSync(filePath, styleSheet, 'utf-8');
         console.log(message + styleSheet);
       }
-    } catch (error) {
-      console.error('Error writing to file:', error);
-      console.error('Stack trace:', error);
+    } else {
+      appendFileSync(filePath, styleSheet, 'utf-8');
+      console.log(message + styleSheet);
     }
+  } catch (error) {
+    console.error('Error writing to file:', error);
+    console.error('Stack trace:', error);
   }
 };
