7d7514f959e5cc5fb829eebc26c43ba75040de91
S.Watanabe <ocelot2828@gmail.com>
2024-09-22 20:48:22 +0900

    perf(custom-html-type.ts): Separated for inference speed MediaQueryType and MediaQueryHTMLType other refactor.

--- src/_internal/types/custom/custom-html-type.ts
+++ src/_internal/types/custom/custom-html-type.ts
@@ -2,27 +2,27 @@ import type { CustomCSSProperties } from './custom-css-properties';
 
 type JSXType = keyof JSX.IntrinsicElements | '*';
 type HTMLType = {
-  [K in JSXType]?: CustomCSSProperties;
+  [K in JSXType]: CustomCSSProperties;
 };
 
 type ClassName = `.${string}`;
 type ClassNameType = {
-  [K in ClassName]?: CustomCSSProperties;
+  [K in ClassName]: CustomCSSProperties;
 };
 
 type Attribute = `${string}[${string}]${string}`;
 type AttributeType = {
-  [K in Attribute]?: CustomCSSProperties;
+  [K in Attribute]: CustomCSSProperties;
 };
 
 type Consecutive = `${JSXType} ${string}`;
 type ConsecutiveType = {
-  [K in Consecutive]?: CustomCSSProperties;
+  [K in Consecutive]: CustomCSSProperties;
 };
 
 type Pseudo = `${JSXType}:${string}`;
 type PseudoType = {
-  [K in Pseudo]?: CustomCSSProperties;
+  [K in Pseudo]: CustomCSSProperties;
 };
 
 type KeyframeSelector = 'from' | 'to' | `${number}%`;
@@ -31,13 +31,17 @@ type KeyframesDefinition = {
   [K in KeyframeSelector]?: CustomCSSProperties;
 };
 
-type AtKeyframes = {
+type KeyframesType = {
   [K in `@keyframes ${string}`]: KeyframesDefinition;
 };
 
 export type MediaQuery = `@media ${string}`;
 export type MediaQueryType = {
-  [key in MediaQuery]?: CustomCSSProperties | CustomHTMLType;
+  [K in MediaQuery]: CustomCSSProperties;
 };
 
-export type CustomHTMLType = HTMLType | ClassNameType | AttributeType | ConsecutiveType | PseudoType | AtKeyframes | MediaQueryType;
+type MediaQueryHTMLType = {
+  [K in MediaQuery]: CustomHTMLType;
+};
+
+export type CustomHTMLType = HTMLType | ClassNameType | AttributeType | ConsecutiveType | PseudoType | KeyframesType | MediaQueryHTMLType;
