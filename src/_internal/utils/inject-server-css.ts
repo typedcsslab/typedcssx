af2599ec0c96babdb19ac74979d43c3b169d6311
S.Watanabe <ocelot2828@gmail.com>
2024-09-27 20:02:19 +0900

    fix(inject-server-css.ts): remove unnecessary return

--- src/_internal/utils/inject-server-css.ts
+++ src/_internal/utils/inject-server-css.ts
@@ -5,8 +5,6 @@ const styleSheets: Record<string, string> = {};
 export function injectServerCSS(hash: string, sheet: string, context: string) {
   if (isDevelopment) console.log('💫 ' + context + ' executing ...' + sheet);
   styleSheets[hash] = sheet;
-
-  return sheet;
 }
 
 export function getServerCSS(): string {
