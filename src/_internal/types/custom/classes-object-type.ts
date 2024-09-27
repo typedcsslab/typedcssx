c40e49afb4acc6d3442d66809da1d671e2000832
S.Watanabe <ocelot2828@gmail.com>
2024-09-27 20:01:38 +0900

    fix(classes-object-type.ts): improve types

--- src/_internal/types/custom/classes-object-type.ts
+++ src/_internal/types/custom/classes-object-type.ts
@@ -1,16 +1,10 @@
 import type { CustomCSSProperties } from './custom-css-properties';
 import { MediaQuery } from './custom-html-type';
 
-export type ClassesObjectType = {
-  [key in MediaQuery]: {
-    [className: string]: CustomCSSProperties;
-  };
-};
-
-type Exact<T, U> = T extends U ? T : never;
+export type ClassesObjectType = Record<MediaQuery, Record<string, CustomCSSProperties>>;
 
 export type ExactClassesObjectType<T> = {
-  [K in keyof T | string]: K extends keyof T ? Exact<T[K], CustomCSSProperties> : CustomCSSProperties;
+  readonly [K in keyof T | string]: K extends keyof T ? Pick<T, K> : CustomCSSProperties;
 };
 
 export type ReturnStyleType<T> = { [key in keyof T]: string };
