3b368b9303456f0239ccd1265930a3a0e56d3ed5
S.Watanabe <ocelot2828@gmail.com>
2024-09-28 16:44:31 +0900

    fix(types): fix Pick<T, K> to T[K] and create Type name is changed.

--- src/_internal/types/index.ts
+++ src/_internal/types/index.ts
@@ -4,7 +4,7 @@ export * from './common/css-values';
 export * from './common/css-variables';
 export * from './common/nth-selectors';
 export * from './common/pseudo-selectors';
-export * from './custom/classes-object-type';
+export * from './custom/create-type';
 export * from './custom/custom-css-properties';
 export * from './custom/custom-html-type';
 export * from './custom/property-type';
