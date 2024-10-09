57a00cdf166fe81a9800d10ef2eb479fd2f75379
S.Watanabe <ocelot2828@gmail.com>
2024-10-09 21:06:21 +0900

    chore(package): update typedcssx-next-plugin at 2.7.2

--- e2e/site/next.config.js
+++ e2e/site/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const { TypedCSSXNextPlugin, configCSSModule } = require('typedcssx-next-plugin');
+const { configCSSModule } = require('typedcssx-next-plugin');
 const nextConfig = {
   webpack: config => {
-    config.plugins.push(new TypedCSSXNextPlugin());
     return configCSSModule(config);
   },
 };
