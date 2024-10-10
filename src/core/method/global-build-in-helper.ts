6b164d97ac9d20133b99ba20a27de1d84d3d6d1b
S.Watanabe <ocelot2828@gmail.com>
2024-10-10 17:17:55 +0900

    feat(build-in-helper): sync to async

--- src/core/method/global-build-in-helper.ts
+++ src/core/method/global-build-in-helper.ts
@@ -2,36 +2,18 @@ import { buildIn, isDevelopment } from '../../_internal';
 
 let resolveGlobalStyleSheet: (value: [string, string?]) => void;
 let globalStyleSheetPromise: Promise<[string, string?]>;
-const styleSheetQueue: [string, string?][] = [];
-let isProcessing = false;
 
 function createGlobalStyleSheetPromise() {
   globalStyleSheetPromise = new Promise<[string, string?]>(resolve => {
-    resolveGlobalStyleSheet = (value: [string, string?]) => {
-      styleSheetQueue.push(value);
-      resolve(value);
-    };
+    resolveGlobalStyleSheet = resolve;
   });
 }
 
-async function executeBuildIn(styleSheet: string, option?: string): Promise<void> {
-  if (!isDevelopment && styleSheet) buildIn(styleSheet, option);
-}
-
-async function processStyleSheets() {
-  while (styleSheetQueue.length > 0) {
-    const [styleSheet, option] = styleSheetQueue.shift() as [string, string?];
-    await executeBuildIn(styleSheet, option);
-  }
-  isProcessing = false;
-}
-
-export function globalBuildIn(): void {
+export async function globalBuildIn(): Promise<void> {
   if (typeof globalStyleSheetPromise === 'undefined') createGlobalStyleSheetPromise();
-  if (!isProcessing && styleSheetQueue.length > 0) {
-    isProcessing = true;
-    processStyleSheets();
-  }
+  const [styleSheet, option] = await globalStyleSheetPromise;
+  if (!isDevelopment && styleSheet) buildIn(styleSheet, option);
+  createGlobalStyleSheetPromise();
 }
 
 export { resolveGlobalStyleSheet, globalStyleSheetPromise, createGlobalStyleSheetPromise };
