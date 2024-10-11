5223dc7f5b437b74dda238635d6cd13869b34775
S.Watanabe <ocelot2828@gmail.com>
2024-10-12 06:11:42 +0900

    fix(union): fix removed null

--- src/core/method/union.ts
+++ src/core/method/union.ts
@@ -1,3 +1,3 @@
-export function union(...classes: Array<string | undefined | null | false>): string {
+export function union(...classes: Array<string | undefined | false>): string {
   return classes.filter(Boolean).join(' ');
 }
