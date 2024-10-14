c6cc0872027818fc3b6b3a63cbb115a22f8d3d64
S.Watanabe <ocelot2828@gmail.com>
2024-10-14 18:10:15 +0900

    fix(build-in.ts): set fs and path to server side only

--- src/_internal/utils/build-in.ts
+++ src/_internal/utils/build-in.ts
@@ -1,17 +1,16 @@
 'use server';
-
-import * as fs from 'fs';
-import * as path from 'path';
 import { isServer } from './helper';
 
-export const buildIn = async (styleSheet: string, global?: string): Promise<void> => {
+export const buildIn = (styleSheet: string, global?: string): void => {
+  if (!isServer) return;
+
+  const fs = require('fs');
+  const path = require('path');
   const styleFilePath = path.join(__dirname, '../../core/styles/style.module.css');
   const globalFilePath = path.join(__dirname, '../../core/styles/global.css');
-
   const filePath = global === '--global' ? globalFilePath : styleFilePath;
   const message = global === '--global' ? ' ✅ Generating global static css \n' : ' ✅ Generating module static css \n';
 
-  if (!isServer) return;
   try {
     if (fs.existsSync(filePath)) {
       const cssData = fs.readFileSync(filePath, 'utf-8');
