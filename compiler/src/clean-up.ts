eb960e4c1339b13af004043a2ae9726740c44067
S.Watanabe <ocelot2828@gmail.com>
2024-10-23 13:14:45 +0900

    fix(clean-up.ts): bug In the range of 3.2.7 to 3.3.0

--- compiler/src/clean-up.ts
+++ compiler/src/clean-up.ts
@@ -3,10 +3,10 @@ import { join } from 'path';
 
 export const cleanUp = async () => {
   const styleFilePath = join(__dirname, '../../dist/core/styles/style.module.css');
-  const globalFilePath = join(__dirname, '../../dist/core/styles/style.module.css');
+  const globalFilePath = join(__dirname, '../../dist/core/styles/global.css');
   try {
-    writeFileSync(styleFilePath, '/*______________________________*/', 'utf-8');
-    writeFileSync(globalFilePath, '/*______________________________*/', 'utf-8');
+    writeFileSync(styleFilePath, '', 'utf-8');
+    writeFileSync(globalFilePath, '', 'utf-8');
   } catch (err) {
     console.error('An error occurred:', err);
   }
